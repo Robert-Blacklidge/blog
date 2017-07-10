a9ef549787ba6b739b74093c09746ec6b4174860
Dues_Ex <robertm.blacklidge@gmail.com>
2017-07-09 22:39:22 -0400

    Added Templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
